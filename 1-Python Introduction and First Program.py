4e31b7f8009803066aa99a93c05d20e881267e7f
Muhammad Haseeb Khan <muhammadhaseeb11801@gmail.com>
2026-08-07 20:52:45 +0500

    Python Introduction and First Program

---
+++ 1-Python Introduction and First Program.py
@@ -0,0 +1,44 @@
+# Python Introduction
+# ============================================
+# 01 - PYTHON INTRO
+# ============================================
+
+# Python is a high-level programming language.
+
+# It is easy to learn and easy to use.
+
+# Python is used for web development,
+
+# data science, machine learning,
+
+# artificial intelligence, and automation.
+
+# print() is a built-in Python function.
+
+
+
+# When the program runs,
+
+print("Hello, World!")
+
+# print() displays the text on the screen.
+
+## Explanation
+
+# It is used to display output on the screen.
+
+# "Hello, World!" is a text string.
+
+## Output
+
+# Hello, World!
+
+## Summary
+
+# This program displays a message on the screen.
+
+# print() is used to show output.
+
+# "Hello, World!" is the first program
+
+# most beginners learn in Python.
